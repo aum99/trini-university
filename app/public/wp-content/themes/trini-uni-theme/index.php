af3f3ff6b0bbb3e579907f5872c0dba17f659779
aum battul <aumbattul99@gmail.com>
2023-08-06 15:08:09 +0530

    adding header-footer and including styles

--- app/public/wp-content/themes/trini-uni-theme/index.php
+++ app/public/wp-content/themes/trini-uni-theme/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php get_header();
 while (have_posts()) {
     the_post(); ?>
     <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
@@ -6,4 +6,5 @@ while (have_posts()) {
     <hr />
 <?php
 }
+get_footer();
 ?>
--- app/public/wp-content/themes/trini-uni-theme/page.php
+++ app/public/wp-content/themes/trini-uni-theme/page.php
@@ -1,9 +1,9 @@
-<?php get_header();
+<?php
 while (have_posts()) {
     the_post(); ?>
+    <h1>This is a page </h1>
     <h2><?php the_title() ?></h2>
     <?php the_content() ?>
 <?php
 }
-get_footer();
 ?>
