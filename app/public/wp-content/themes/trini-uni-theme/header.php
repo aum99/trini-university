ce03dba458ca221a9f40997e4d1fa8f932f5f1ec
aum battul <aumbattul99@gmail.com>
2023-08-07 00:55:45 +0530

    adding routes for login and register page

--- app/public/wp-content/themes/trini-uni-theme/header.php
+++ app/public/wp-content/themes/trini-uni-theme/header.php
@@ -15,7 +15,8 @@
             <h1 class="school-logo-text float-left">
                 <a href="<?php echo site_url('/') ?>"><strong>Trini</strong> University</a>
             </h1>
-            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
+                    aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
                 <nav class="main-navigation">
@@ -26,9 +27,12 @@
                     </ul>
                 </nav>
                 <div class="site-header__util">
-                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                    <a href="<?php echo site_url('/login') ?>"
+                        class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="<?php echo site_url('/register') ?>"
+                        class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search"
+                            aria-hidden="true"></i></span>
                 </div>
             </div>
         </div>
